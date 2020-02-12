845a83b6dfe43210c8ee569cbad581181e6b76dd
Mike <hockeydad66@live.com>
2020-02-11 20:27:28 -0700

    WOrking code to pull and push required parameters

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ const mongoose = require('mongoose');
 const cheerio = require('cheerio');
 const axios = require('axios');
 
-var PORT = 3000;
+var PORT = process.env.PORT || 8080;
+var databaseURL = "news";
+var collections = ["articles"];
 
 //Handlebars package and initiation
 const exhb = require('express-handlebars');
 
-var db = require("./models");
+ const db = require("./models/");
 
 //Initialize Express
 const app = express();
@@ -23,8 +25,41 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
-//Connecting to mongo database
-mongoose.connect("mongodb://localhost/newsPopulater", { useNewUrlParser: true});
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/news", { useNewUrlParser: true });
+
+//Scrape articles from website and insert into mongo database
+app.get("/scrape", function(req, res) {
+    axios.get("https://www.nytimes.com/section/sports").then(function(response) {
+        var $ =cheerio.load(response.data);
+        $("article").each(function(i, element) {
+            var title = $(element).find("h2").text();
+            var summary = $(element).find("p").text();
+            var link = $(element).find("a").attr("href");
+
+            console.log(title, link);
+
+            if(title && link) {
+                db.Articles.create({
+                    title: title,
+                    summary: summary,
+                    link: link
+                },
+                function(err, inserted) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        console.log(inserted);
+                    }
+                });
+            }
+        });
+    });
+
+    res.send("Scrape complete");
+});
+
 
 // Start the server
 app.listen(PORT, function() {
